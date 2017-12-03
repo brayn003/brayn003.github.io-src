8c11507876c4f829ecb75a171809da11e78a0d6c
brayn003 <brayn003@gmail.com>
2017-12-03 18:48:00 +0530

    adding tags on homepage

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -5,7 +5,14 @@ import './style.scss';
 export default function Home(){
   return (
     <div className="view-home">
-      <p className="home-title">brayn003</p>  
+      <div className="home-container">
+        <p className="home-title">brayn003</p>
+        <div>
+          <p className="home-tags"><span className="typcn typcn-pencil"></span>&nbsp;Designer</p>
+          <p className="home-tags"><span className="typcn typcn-lightbulb"></span>&nbsp;Ideator</p>
+          <p className="home-tags"><span className="typcn typcn-device-desktop"></span>&nbsp;Developer</p>
+        </div>
+      </div>
     </div>
   );
 }
